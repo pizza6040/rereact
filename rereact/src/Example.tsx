847ce2954ffdf6c1571e0c76524fb21298d53175
pizza6040 <red_fruit_0604@icloud.com>
2025-03-22 01:21:49 +0900

    routing

---
+++ rereact/src/Example.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
+const Example = () => {
+  return (
+    <div>
+      <div>About</div>
+    </div>
+  );
+};
+
+export default Example;
